916494be2fa0366f642b3b5fec2e7d523bb0b23d
gAb09 <brunogabiot@club-internet.fr>
2016-04-17 20:12:25 +0200

    Ajout packages collective html et form

--- config/app.php
+++ config/app.php
@@ -155,7 +155,11 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /*
+         * Packages Service Providers...
+         */
+        Menus\MenusServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -201,6 +205,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
