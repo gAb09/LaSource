50c8a9a0bf41ad8c28204009c6739e13fdf98294
gAb09 <brunogabiot@club-internet.fr>
2016-06-18 17:58:18 +0200

    correctifs dans ProducteurController

--- app/Http/Controllers/ProducteurController.php
+++ app/Http/Controllers/ProducteurController.php
@@ -22,9 +22,8 @@ class ProducteurController extends Controller
 
     public function create()
     {
-        // $item = producteur::where('id', $id)->first();
-        $titre_page = 'Création d’un producteur';
         $item =  new Producteur;
+        $titre_page = 'Création d’un producteur';
 
         return view('producteur.create')->with(compact('titre_page', 'item'));
     }
@@ -33,28 +32,27 @@ class ProducteurController extends Controller
     public function store(ProducteurRequest $request)
     {
         $item = new Producteur;
-                $item->id = $request->id;
-                $item->exploitation = $request->exploitation;
-                $item->nom = $request->nom;
-                $item->prenom = $request->prenom;
-                $item->ad1 = $request->ad1;
-                $item->ad2 = $request->ad2;
-                $item->cp = $request->cp;
-                $item->ville = $request->ville;
-                $request->tel = str_replace('.', '', $request->tel);
-                $request->tel = str_replace(' ', '', $request->tel);
-                $item->tel = $request->tel;
-                $request->mobile = str_replace('.', '', $request->mobile);
-                $request->mobile = str_replace(' ', '', $request->mobile);
-                $item->mobile = $request->mobile;
-                $item->email = $request->email;
-                $item->nompourpaniers = $request->nompourpaniers;
-                $item->is_actif = (isset($request->is_actif)?1:0);
-                if($item->save()){
-                    return redirect()->route('producteur.index')->with('success', 'Le producteur a bien été créé');
-                }else{
-                    return redirect()->back()->with('status', 'Problème lors de la création');
-                }
+        $item->exploitation = $request->exploitation;
+        $item->nom = $request->nom;
+        $item->prenom = $request->prenom;
+        $item->ad1 = $request->ad1;
+        $item->ad2 = $request->ad2;
+        $item->cp = $request->cp;
+        $item->ville = $request->ville;
+        $request->tel = str_replace('.', '', $request->tel);
+        $request->tel = str_replace(' ', '', $request->tel);
+        $item->tel = $request->tel;
+        $request->mobile = str_replace('.', '', $request->mobile);
+        $request->mobile = str_replace(' ', '', $request->mobile);
+        $item->mobile = $request->mobile;
+        $item->email = $request->email;
+        $item->nompourpaniers = $request->nompourpaniers;
+        $item->is_actif = (isset($request->is_actif)?1:0);
+        if($item->save()){
+            return redirect()->route('producteur.index')->with('success', trans('message.producteur.storeOk'));
+        }else{
+            return redirect()->back()->with('status', trans('message.producteur.storefailed'));
+        }
 
     }
 
@@ -73,35 +71,40 @@ class ProducteurController extends Controller
         // return dd($request->all());
     	// return "update : producteur n° $id";
     	$item = producteur::where('id', $id)->first();
-                $item->exploitation = $request->exploitation;
-                $item->nom = $request->nom;
-                $item->prenom = $request->prenom;
-                $item->ad1 = $request->ad1;
-                $item->ad2 = $request->ad2;
-                $item->cp = $request->cp;
-                $item->ville = $request->ville;
-                $request->tel = str_replace('.', '', $request->tel);
-                $request->tel = str_replace(' ', '', $request->tel);
-                $item->tel = $request->tel;
-                $request->mobile = str_replace('.', '', $request->mobile);
-                $request->mobile = str_replace(' ', '', $request->mobile);
-                $item->mobile = $request->mobile;
-                $item->email = $request->email;
-                $item->nompourpaniers = $request->nompourpaniers;
-                $item->is_actif = (isset($request->is_actif)?1:0);
-                if($item->save()){
-                    return redirect()->route('producteur.index')->with('success', 'Le producteur a bien été créé');
-                }else{
-                    return redirect()->back()->with('status', 'Problème lors de la création');
-                }
+        $item->exploitation = $request->exploitation;
+        $item->nom = $request->nom;
+        $item->prenom = $request->prenom;
+        $item->ad1 = $request->ad1;
+        $item->ad2 = $request->ad2;
+        $item->cp = $request->cp;
+        $item->ville = $request->ville;
+        $request->tel = str_replace('.', '', $request->tel);
+        $request->tel = str_replace(' ', '', $request->tel);
+        $item->tel = $request->tel;
+        $request->mobile = str_replace('.', '', $request->mobile);
+        $request->mobile = str_replace(' ', '', $request->mobile);
+        $item->mobile = $request->mobile;
+        $item->email = $request->email;
+        $item->nompourpaniers = $request->nompourpaniers;
+        $item->is_actif = (isset($request->is_actif)?1:0);
+
+        if($item->save()){
+            return redirect()->route('producteur.index')->with('success', trans('message.producteur.updateOk'));
+        }else{
+            return redirect()->back()->with('status', trans('message.producteur.updatefailed'));
+        }
     }
 
     public function destroy($id)
     {
         $item = producteur::where('id', $id)->first();
-        $item->delete();
+        
+        if($item->delete()){
+            return redirect()->route('producteur.index')->with('success', trans('message.producteur.deleteOk'));
+        }else{
+            return redirect()->back()->with('status', trans('message.producteur.deletefailed'));
+        }
 
-        return redirect()->route('producteur.index');
     }
 
 
