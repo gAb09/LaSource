63d8dd7c15131a044fb2b49a0e312029e254ff9c
gAb09 <brunogabiot@club-internet.fr>
2016-06-18 17:55:05 +0200

    Ajout de la traduction locale des messsages crud

---
+++ resources/lang/fr/message.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages La Source
+    |--------------------------------------------------------------------------
+    |
+    | ?????????????????????.
+    |
+    */
+
+    'client' => [
+        'updatecoordOk'     => 'Vos coordonnées ont bien été modifiées.',
+        'updatecoordfailed' => 'Problème. Vos coordonnées n\ont pas été mises à jour…',
+        'updateidentOk'     => 'Vos identifiants ont bien été modifiés.',
+        'updateidentfailed' => 'Problème. Vos identifiants n\ont pas été mis à jour…',
+        'storeOk'           => 'Le client a bien été créé.',
+        'storefailed'       => 'Problème. Le client n’a pas pu être créé…',
+        'deleteOk'           => 'Le client a bien été supprimé.',
+        'deletefailed'       => 'Problème. Le client n’a pas pu être supprimé…',
+    ],
+    'producteur' => [
+        'updateOk'      => 'updateOk.',
+        'updatefailed'  => 'updatefailed.',
+        'storeOK'       => 'Le producteur a bien été créé.',
+        'storefailed'   => 'Problème. Le producteur n’a pas pu être créé…',
+        'deleteOk'      => 'Le producteur a bien été supprimé.',
+        'deletefailed'  => 'Problème. Le producteur n’a pas pu être supprimé…',
+    ],
+
+    ];
