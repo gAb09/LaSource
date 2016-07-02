2f35e791fb2ba47ef1d15e04f4666a402cd92f5d
gAb09 <brunogabiot@club-internet.fr>
2016-07-02 15:42:23 +0200

    Ajout JQuery UI en local + bascule sur son JQuery local

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,8 +114,14 @@
 
     @include('layouts.footer')
 
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <!-- JQuery UI (local) -->
+    <script src="{{asset('js/jquery-ui-1.11.4.custom/jquery-ui.min.css')}}"></script>
+
+    <!-- JQuery (local) -->
+    <script src="{{asset('js/jquery-ui-1.11.4.custom/external/jquery/jquery.js')}}"></script>
+
+    <!-- JavaScripts 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script> -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
     @section('script')
