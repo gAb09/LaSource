d8ad60d478f9b739674410fe5fdfe19dbbd568cb
gAb09 <brunogabiot@club-internet.fr>
2016-08-05 00:47:12 +0200

    Factorisation du mesage vlaidation après setrangs

--- app/Domaines/Domaine.php
+++ app/Domaines/Domaine.php
@@ -13,6 +13,13 @@ class Domaine
 	}
 
 
+	public function getModelName()
+	{
+		$name = explode("\\", get_class($this->model));
+		return lcfirst(array_pop($name));
+	}
+
+
 	public function all($order = 'id')
 	{
 		return $this->model->orderBy($order)->get();
@@ -44,19 +51,19 @@ class Domaine
 
 	public function setRangs($request)
 	{
+		$model_name = $this->getModelName();
 		$tablo = $request->get('tablo');
-
 		foreach ($tablo as $doublet) {
 			$id = $doublet[0];
 			$rang = $doublet[1];
 
-			if(! $panier = $this->model->find($id)){
-				return '<div class="alert alert-danger">'.trans('message.panier.setRangsFailed').'</div>';
+			if(! $model = $this->model->find($id)){
+				return '<div class="alert alert-danger">'.trans("message.$model_name.setRangsFailed").'</div>';
 			}
-			$panier->rang = $rang;
-			$panier->save();
+			$model->rang = $rang;
+			$model->save();
 		}
-		return '<div class="alert alert-success">'.trans('message.panier.setRangsOk').'</div>';
+		return '<div class="alert alert-success">'.trans("message.$model_name.setRangsOk").'</div>';
 	}
 
 }
