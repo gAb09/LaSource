2e2c8fe4f9ad9379fa9c8040265fab051c93fa1c
gAb09 <brunogabiot@club-internet.fr>
2016-09-06 00:46:13 +0200

    Livraison • réécriture mode d'emploi

--- resources/views/livraison/modemploi.blade.php
+++ resources/views/livraison/modemploi.blade.php
@@ -1,11 +1,15 @@
 <h4>Créer/modifier une livraison :</h4>
 <ol>
-	<li>Assigner les 3 dates, puis les valider.</li>
-	<li>Ajouter/retirer des paniers grâce à la liste des paniers (&nbsp;<i class="fa fa-btn fa-shopping-basket"></i>) ou individuellement (&nbsp;<i class="fa fa-btn fa-unlink"></i>).</li>
-	<li>Ensuite et pour chaque panier, indiquer le producteur.<br />
-		Il est possible de modifier les producteurs liés depuis cette page (&nbsp;<i class="fa fa-btn fa-edit"></i>sous la sélection du producteur).</li>
-	<li>Si le prix du panier diffère du prix de base, saisir une valeur dans le champs “Prix livraison”.<br />
-		Sinon cliquer sur le champ prix base pour reporter sa valeur.</li>
-	<li>Valider les paniers. Il est alors possible de passer à la gestion des mails.</li>
-	<li>Nota : il est possible de modifier directement depuis cette page la fiche d'un panier,<br />(&nbsp;<i class="fa fa-btn fa-edit"></i>à gauche de chaque panier).</li>
+	<li><strong>Assigner les 3 dates</strong>, puis les valider.</li>
+	<li><strong>Ajouter/retirer des paniers</strong> grâce à la liste des paniers (&nbsp;<i class="fa fa-btn fa-shopping-basket"></i>) ou individuellement (&nbsp;<i class="fa fa-btn fa-unlink"></i>).</li>
+	<li>Ensuite et pour chaque panier, <strong>indiquer le producteur.</strong><br />
+		Il est possible de modifier la liste des producteurs pour chaque panier : (&nbsp;<i class="fa fa-btn fa-edit"></i>sous la sélection du producteur).</li>
+	<li>À la création d’une livraison<strong>les prix des paniers</strong> sont assignés à 0.<br />
+		Pour assigner le prix de base à cette livraison, cliquer sur le champs “Prix de base”.<br />
+		Pour assigner une valeur spécifique saisir une nouvelle valeur dans le champs “Prix livraison”.</li>
+		<li>Valider les paniers.</li>
+	<li style="color:red">Gestion des relais….</li>
+	<li style="color:red">Gestion des modes de paiement.</li>
+	<li style="color:red">Il est alors possible de passer à la gestion des mails.</li>
+	<p>Nota : il est possible de modifier directement depuis cette page la fiche d'un panier,<br />(&nbsp;<i class="fa fa-btn fa-edit"></i>à gauche de chaque panier).</p>
 </ol>
