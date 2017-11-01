b3f3f9fa17abe030ed719ef27ffdcc68d45377e6
gAb09 <brunogabiot@club-internet.fr>
2017-11-01 13:43:49 +0100

    • GLOBAL • factorisation du champ 'is_actived' dans les formulaires d'édition des entités 'relais', 'producteur', 'modespaiement' et 'panier' via une vue 'shared'.

--- resources/views/modepaiement/form.blade.php
+++ resources/views/modepaiement/form.blade.php
@@ -33,13 +33,4 @@
                         </div>
 
                         <!-- is_actived -->
-                        <div id="" class="form-group toggle_actived {{$model->class_actif}}">
-                            <label class="col-md-4 control-label">Activé</label>
-                            <div class="col-md-6">
-                                <input type="checkbox" class="form-control" style="width:16px" name="is_actived" onChange="javascript:handleIsActivedClass();" 
-                                @if($model->is_actived or old('is_actived'))
-                                checked="checked" 
-                                @endif
-                                value="{{ $model->is_actived or old('is_actived') }}">
-                            </div>
-                        </div>
+                        @include('shared.actived')
